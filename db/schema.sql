f1027d095406aed8df6c0f221f49970e72b889eb
Caroline <Cluce059@gmail.com>
2021-09-09 16:44:32 -0400

    db set up w seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,28 +1,28 @@
-DROP TABLE IF EXISTS departments;
+/*DROP TABLE IF EXISTS departments;
 DROP TABLE IF EXISTS roles;
-
+DROP DATABASE IF EXISTS employees;
+*/
 CREATE TABLE departments (
-    department_id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30)
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL,
-    department_id INTEGER,
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10, 2) NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
-/*id whats goin on w this manager fk yet*/
-/*CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
-/*
 CREATE TABLE employees (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id),
-    CONSTRAINT fk_manager FOREIGN KEY (role.title_id) REFERENCES roles(title) DEFAULT  
-); */
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE
+    /*FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE CASCADE*/
+); 
 
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,9 +1,17 @@
 INSERT INTO departments (name)
 VALUES
-('Bakery'), ('Pharmacy'), ('Produce'), ('Meat');
+('Sales'), ('Customer Service'), ('Legal'), ('Engineering');
 
-INSERT INTO roles (title, salary)
+INSERT INTO roles (title, salary, department_id)
+VALUES ('Sales Rep', 25.40, 1), ('Receptionist', 20.00, 2), ('Team Council', 75.50, 3), ('Engineer', 50.00, 1);
+
+/*manager id references the employee id of a manager ie a role_id of 4 so carol u r manager today! underlings will have a manager_id of 2 in this case*/
+/*no manager id for bossman bc he has no manager*/
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES 
-('Cashier','1010'), ('Bagger', '1010'), ('Cashier','1010'), 
-('Manager', '1800'), ('Pharmacy_technician', '2025');
+('Joe', 'Shmoe', 1, 3),
+ ('Jay', 'Say', 1, 3), 
+ ('Peyton', 'Manning', 4, null), 
+ ('Jody', 'Beehive', 2, 5), 
+ ('Larry', 'Legal', 3, null);
 
